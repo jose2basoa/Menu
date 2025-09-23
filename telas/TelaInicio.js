b28793f1c122d4c7357070cb11e4072131069a4d
Silvio Junior <2real121@gmail.com>
2025-09-23 20:33:38 -0300

    criacao de telas e menu lateral

---
+++ telas/TelaInicio.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import { Button } from 'react-native-web';
+
+function TelaInicio() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Bem-vindo à Tela Inicial!</Text>
+        </View>
+    );
+}
+
+export default TelaInicio;
